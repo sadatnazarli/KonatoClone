7d5d4cb18764c27f480e577a5daeddbe00a5735e
Sadat <sedat.nazarli@gmail.com>
2024-04-11 18:08:28 +0400

    added new cards with carusel

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,36 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Function to close all select boxes, except the current one
-  function closeAllSelect(elmnt) {
-    var x,
-      y,
-      i,
-      arrNo = [];
-    x = document.getElementsByClassName("select-items");
-    y = document.getElementsByClassName("select-selected");
-    for (i = 0; i < y.length; i++) {
-      if (elmnt == y[i]) {
-        arrNo.push(i);
-      } else {
-        y[i].classList.remove("select-arrow-active");
-      }
-    }
-    for (i = 0; i < x.length; i++) {
-      if (arrNo.indexOf(i)) {
-        x[i].classList.add("select-hide");
-      }
-    }
+var carousel = document.getElementById("carousel");
+var scrollAmount = 0;
+
+function autoScroll() {
+  carousel.scrollTo(++scrollAmount, 0);
+
+  if (scrollAmount != carousel.scrollWidth) {
+    requestAnimationFrame(autoScroll);
   }
+}
 
-  // Toggle between hiding and showing the dropdown content
-  document.querySelectorAll(".select-selected").forEach(function (select) {
-    select.addEventListener("click", function (e) {
-      e.stopPropagation();
-      closeAllSelect(this);
-      this.nextSibling.classList.toggle("select-hide");
-      this.classList.toggle("select-arrow-active");
-    });
-  });
-
-  // If the user clicks anywhere outside the select box, then close all select boxes
-  document.addEventListener("click", closeAllSelect);
-});
+autoScroll();
