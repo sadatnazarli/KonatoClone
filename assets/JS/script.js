1f25e4d1f40c25e471f9c3136ed29c94e8790f49
Sadat <sedat.nazarli@gmail.com>
2024-04-13 12:49:28 +0400

    finished first slider

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,12 +1,24 @@
-var carousel = document.getElementById("carousel");
-var scrollAmount = 0;
+document.addEventListener('DOMContentLoaded', function() {
+  const slider = document.querySelector('.slider-wrapper .image-list');
+  const prevButton = document.getElementById('prev-slide');
+  const nextButton = document.getElementById('next-slide');
 
-function autoScroll() {
-  carousel.scrollTo(++scrollAmount, 0);
-
-  if (scrollAmount != carousel.scrollWidth) {
-    requestAnimationFrame(autoScroll);
+  function updateButtons() {
+      prevButton.style.visibility = slider.scrollLeft > 0 ? 'visible' : 'hidden';
+      const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
+      nextButton.style.visibility = slider.scrollLeft < maxScrollLeft ? 'visible' : 'hidden';
   }
-}
 
-autoScroll();
+  prevButton.onclick = function() {
+      slider.scrollLeft -= slider.clientWidth;
+  };
+
+  nextButton.onclick = function() {
+      slider.scrollLeft += slider.clientWidth;
+  };
+
+  slider.addEventListener('scroll', updateButtons);
+  updateButtons(); 
+
+  window.addEventListener('resize', updateButtons); 
+});
